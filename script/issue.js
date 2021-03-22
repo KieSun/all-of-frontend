4340f25ec5a881306c3363f3b1ae549fe1754bd2
xuwu <zx597813039@gmail.com>
2021-03-22 21:59:51 +0800

    ci: action

--- script/issue.js
+++ script/issue.js
@@ -1,12 +1,20 @@
+const axios = require('axios')
 const { event: { issue, action } } = JSON.parse(process.env.GITHUB_CONTEXT)
 const { author_association, body, title, number } = issue
-
-console.log(process.env.ISSUEHOST)
+const host = process.env.ISSUEHOST
 
 if (author_association === 'OWNER') {
     if (action === 'edited') {
-        console.log(body, title, number, '----edited')
+        axios.put(host, {
+            name: title,
+            issueId: number,
+            content: body
+        })
     } else if (action === 'opened') {
-        console.log(body, title, number, '----opened')
+        axios.post(host, {
+            name: title,
+            issueId: number,
+            content: body
+        })
     }
 }
